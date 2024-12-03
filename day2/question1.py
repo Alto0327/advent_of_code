b8ab49c686eef4760ca81bc2ec915056bd99efd4
Alto0327 <aj.silvaf2003@gmail.com>
2024-12-02 19:26:41 -0500

    Day 2 only question 1

---
+++ day2/question1.py
@@ -0,0 +1,27 @@
+def safety_count(reports ):
+    count = 0
+
+    for list_index, lists in enumerate(reports):
+        
+        increase_res = all(i < j for i, j in zip(lists, lists[1:]))
+        decrease_res = all(i > j for i, j in zip(lists, lists[1:]))
+        
+        valid_differences = all(1 <= abs(lists[i] - lists[i + 1]) <= 3 for i in range(len(lists) - 1))
+
+        if (increase_res or decrease_res) and valid_differences:
+                count += 1
+
+        print("List", list_index, "completed")
+
+    return count 
+
+# Read File and create lists for each line in data file
+reports = [] 
+
+with open ("data.txt", "r") as file:
+    for line in file:
+        reports.append(list(map(int, line.rstrip().split())))
+
+x = safety_count(reports)
+
+print(x)
